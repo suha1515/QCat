f68ee5a8117242270036db9b6a4b1aec7cc33b3c
suha1515 <suha1515@naver.com>
2021-02-15 08:58:48 +0900

    various post-effect code inversion,grayscale,sharpen,blur,edge-detection * but hlsl doenst work when using kernel effect

--- SandBox/Asset/shaders/glsl/SingleQuad.glsl
+++ SandBox/Asset/shaders/glsl/SingleQuad.glsl
@@ -21,8 +21,52 @@ layout(location = 0) out vec4 color;
 in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
-
+const float offsetX = 1.0 / 1600.0;  
+const float offsetY = 1.0 / 900.0;
 void main()
 {
-	color = texture(screenTexture,TexCoords);
+	vec2 offsets[9] = vec2[](
+        vec2(-offsetX,  offsetY), // top-left
+        vec2( 0.0f,    offsetY), // top-center
+        vec2( offsetX,  offsetY), // top-right
+        vec2(-offsetX,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offsetX,  0.0f),   // center-right
+        vec2(-offsetX, -offsetY), // bottom-left
+        vec2( 0.0f,   -offsetY), // bottom-center
+        vec2( offsetX, -offsetY)  // bottom-right    
+    );
+
+	// sharpen
+   //float kernel[9] = float[](
+   //    -1, -1, -1,
+   //    -1,  9, -1,
+   //    -1, -1, -1
+   // );
+   // blur
+	//float kernel[9] = float[](
+   // 1.0 / 16, 2.0 / 16, 1.0 / 16,
+   // 2.0 / 16, 4.0 / 16, 2.0 / 16,
+   // 1.0 / 16, 2.0 / 16, 1.0 / 16  
+	//);
+	// edge detection
+	float kernel[9] = float[](
+	    1,  1, 1,
+	    1, -8, 1,
+	    1, 1,  1
+	 );
+	vec4 FragColor = texture(screenTexture,TexCoords);
+	//Inversion
+	//color = vec4(vec3(1.0 - texture(screenTexture,TexCoords)),1.0);
+	// GrayScale
+	//float average =  0.2126 * FragColor.r +0.7152 * FragColor.g +  0.0722 * FragColor.b;
+    //color = vec4(average, average, average, 1.0);
+	// Kernels Sharpen
+	vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+    {
+        col += vec3(texture(screenTexture, TexCoords.st + offsets[i]))* kernel[i];
+    }
+    color = vec4(col,1.0);
+	//color = texture(screenTexture,TexCoords);
 }
--- SandBox/Asset/shaders/hlsl/SingleQuad_PS.hlsl
+++ SandBox/Asset/shaders/hlsl/SingleQuad_PS.hlsl
@@ -1,7 +1,60 @@
 
 Texture2D screenTex;
 SamplerState splr :register(s0);
+const float offsetX =1.0f/1600.0f;
+const float offsetY = 1.0f / 900.0f;
 float4 main(float2 tc : Texcoord) : SV_TARGET
 {
-	return screenTex.Sample(splr,tc);
+	float4 color;
+	float4 texColor = screenTex.Sample(splr, tc);
+	
+	//Inversion
+	//color = float4(float3(1.0f-screenTex.Sample(splr, tc).rgb), 1.0f);
+	// GrayScale
+	//float average = ((0.2126 *texColor.r) + (0.7152 * texColor.g)+ (0.0722 * texColor.b));
+	//color = float4(average, average, average, 1.0f);
+	//Kernels
+	float2 offsets[9] = 
+	{
+		float2(-offsetX, offsetY),	//top - left
+		float2(0.0f, offsetY),	 //top - center
+		float2(offsetX, offsetY),  //top - right
+		float2(-offsetX, 0.0f),	 //center-left
+		float2(0.0f, 0.0f),		 //center-center
+		float2(offsetX, 0.0f),	 //center-right
+		float2(-offsetX, -offsetY), //bottom - left
+		float2(0.0f, -offsetY),	 //bottom - center
+		float2(offsetX, -offsetY)  //bottom-right
+	};
+
+	/*float kernel[9] = {
+		-1, -1, -1,
+		-1,  9, -1,
+		-1, -1, -1
+	};*/
+	float kernel[9]=
+	{
+		1.0f / 16.0f , 2.0f / 16.0f, 1.0f / 16.0f,
+		2.0f / 16.0f , 4.0f / 16.0f, 2.0f / 16.0f,
+		1.0f / 16.0f , 2.0f / 16.0f, 1.0f / 16.0f
+	};
+	float3 sampleTex[9];
+	/*sampleTex[0] = screenTex.Sample(splr, tc + float2(-offset, offset)).rgb;
+	sampleTex[1] = screenTex.Sample(splr, tc + float2( 0.0f, offset)).rgb;
+	sampleTex[2] = screenTex.Sample(splr, tc + float2(offset, offset)).rgb;
+	sampleTex[3] = screenTex.Sample(splr, tc + float2(-offset, 0.0f)).rgb;
+	sampleTex[4] = screenTex.Sample(splr, tc + float2(0.0f, 0.0f)).rgb;
+	sampleTex[5] = screenTex.Sample(splr, tc + float2( offset, 0.0f)).rgb;
+	sampleTex[6] = screenTex.Sample(splr, tc + float2(-offset, -offset)).rgb;
+	sampleTex[7] = screenTex.Sample(splr, tc + float2( 0.0f, -offset)).rgb;
+	sampleTex[8] = screenTex.Sample(splr, tc + float2(offset, -offset)).rgb;*/
+	float3 col = float3(0.0f, 0.0f, 0.0f);
+	[unroll]
+	for (int i = 0; i < 9; ++i)
+	{
+		col += screenTex.Sample(splr, tc + offsets[i]).rgb * kernel[i];
+		//sampleTex[i] = float3(1.0f, 1.0f, 1.0f);
+	}
+	color = float4(col, 1.0f);
+	return color;
 }
